2c3558e50d7f687de7810adb963d94bf136b2460
Intheautumn <695384241@qq.com>
2021-11-23 14:56:29 +0800

    提交分支

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let config = {
     // 游戏的状态  0:游戏未开始  1：游戏进行中   2: 游戏结束
     status: 0,
     // 病毒生成的时间间隔
-    interval:800,
+    interval: 800,
     // 病毒动画的速度
-    speed:3
+    speed: 3
 }
 
 // 得分
@@ -12,37 +12,38 @@ let score = 0;
 
 // 开始页面
 let startAlert = document.querySelector('#start-alert')
-// logo
+    // logo
 let gameDesc = document.querySelector('.game-desc')
 
 let footer = document.querySelector('#start-alert footer')
 
-startAlert.onclick = function(){
+startAlert.onclick = function() {
     console.log('游戏开始')
 
     gameDesc.classList.add('slide-up')
     footer.classList.add('slide-down')
 
-    setTimeout(function(){
+    setTimeout(function() {
         startAlert.style.display = 'none'
-    },500)
+    }, 500)
     startGame()
 
     // 更新游戏状态
     config.status = 1;
 }
 
-var timer,updater;
-function startGame(){
+var timer, updater;
 
-    timer = setInterval(function(){
+function startGame() {
+
+    timer = setInterval(function() {
         makeVirus()
-    },config.interval)
+    }, config.interval)
 
-    updater = setInterval(function(){
+    updater = setInterval(function() {
         update()
-    },16)
-    
+    }, 16)
+
 }
 
 let game = document.querySelector('#game')
@@ -58,35 +59,41 @@ let stage = document.querySelector("#stage")
 let uiLayer = document.querySelector('#ui')
 
 let virues = []
-// 生成病毒元素
-function makeVirus(){
+    // 生成病毒元素
+function makeVirus() {
     let virus = document.createElement('div')
-    virus.setAttribute('class','virus')
+    virus.setAttribute('class', 'virus')
     let p = document.createElement('p')
     p.classList.add('letter')
 
     virus.appendChild(p)
 
     // 设置病毒的颜色
-    switch(Math.floor(Math.random() * 6)){
+    switch (Math.floor(Math.random() * 6)) {
         case 0:
             p.style.backgroundImage = 'radial-gradient(rgba(255,150,150,0),rgba(255,0,0,1))';
-            p.style.boxShadow = '0 0 15px #f00';break;
+            p.style.boxShadow = '0 0 15px #f00';
+            break;
         case 1:
             p.style.backgroundImage = 'radial-gradient(rgba(0, 255, 0, 0),rgba(0,255,0,1))';
-            p.style.boxShadow = '0 0 15px #f00'; break;
+            p.style.boxShadow = '0 0 15px #f00';
+            break;
         case 2:
             p.style.backgroundImage = 'radial-gradient(rgba(0, 0, 255, 0),rgba(0,0,255,1))';
-            p.style.boxShadow = '0 0 15px #f00'; break;
+            p.style.boxShadow = '0 0 15px #f00';
+            break;
         case 3:
             p.style.backgroundImage = 'radial-gradient(rgba(255, 255, 0, 0),rgba(255,255,0,1))';
-            p.style.boxShadow = '0 0 15px #f00'; break;
+            p.style.boxShadow = '0 0 15px #f00';
+            break;
         case 4:
             p.style.backgroundImage = 'radial-gradient(rgba(0, 255, 255, 0),rgba(0,255,255,1))';
-            p.style.boxShadow = '0 0 15px #f00'; break;
+            p.style.boxShadow = '0 0 15px #f00';
+            break;
         case 5:
             p.style.backgroundImage = 'radial-gradient(rgba(255, 0, 255, 0),rgba(255,0,255,1))';
-            p.style.boxShadow = '0 0 15px #f00'; break;
+            p.style.boxShadow = '0 0 15px #f00';
+            break;
     }
 
 
@@ -105,31 +112,31 @@ function makeVirus(){
 let winH = stage.offsetHeight;
 
 // update 动画，刷新元素的位置
-function update(){
+function update() {
 
-    for(let i = 0;i < virues.length;i++){
+    for (let i = 0; i < virues.length; i++) {
         let virus = virues[i];
         virus.style.top = virus.offsetTop + config.speed + 'px'
 
-        if(virus.offsetTop > (winH - 200) && !uiLayer.warning ){
+        if (virus.offsetTop > (winH - 200) && !uiLayer.warning) {
             showWarning()
             uiLayer.warning = true;
-        }else if(virus.offsetTop >= winH){
+        } else if (virus.offsetTop >= winH) {
             // game over
             gameOver()
         }
     }
 }
 
-function showWarning(){
+function showWarning() {
     let warningLayer = document.createElement('div')
-    warningLayer.setAttribute('class','warning')
+    warningLayer.setAttribute('class', 'warning')
     uiLayer.appendChild(warningLayer)
 }
 
 let gameOverAlert = document.querySelector('#game-over-alert')
-// 游戏介绍
-function gameOver(){
+    // 游戏介绍
+function gameOver() {
     clearInterval(timer)
     clearInterval(updater)
     config.status = 2;
@@ -142,13 +149,13 @@ let scoreLabel = document.getElementById('score-label')
 let xmEffect = document.querySelector('#xm')
 
 // 监听键盘事件
-window.addEventListener('keyup',function(e){
+window.addEventListener('keyup', function(e) {
     let key = e.key;
 
-    for(let i = 0;i < virues.length;i++){
+    for (let i = 0; i < virues.length; i++) {
         let virus = virues[i]
 
-        if(virus.letter.toLowerCase() === key.toLocaleLowerCase() && config.status === 1){
+        if (virus.letter.toLowerCase() === key.toLocaleLowerCase() && config.status === 1) {
 
             // 切换病毒
             let dieImg = document.createElement('img')
@@ -159,11 +166,11 @@ window.addEventListener('keyup',function(e){
             dieImg.style.top = virus.offsetTop + 'px'
             dieImg.classList.add('fade-out')
 
-            setTimeout(function(){
+            setTimeout(function() {
                 game.removeChild(dieImg)
-            },1000)
+            }, 1000)
             game.removeChild(virus)
-            virues.splice(i,1)
+            virues.splice(i, 1)
 
 
             score++;
@@ -180,12 +187,12 @@ window.addEventListener('keyup',function(e){
 
 // 重玩
 let restartBtn = document.querySelector('#restart-btn')
-restartBtn.onclick = function(){
+restartBtn.onclick = function() {
     gameOverAlert.style.display = 'none'
     resetGame()
 }
 
-function resetGame(){
+function resetGame() {
     config.status = 1;
     score = 0;
     scoreLabel.innerHTML = score;
